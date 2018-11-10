b6bab0d985188aeb6bb3b454552a81b0db429fb8
Anna Fedoruk <annafedoruk.af@gmail.com>
2018-11-10 20:11:27 +0100

    first commit

--- main.js
+++ main.js
@@ -11,16 +11,22 @@ $(document).ready(function () {
                         $('.dead__three').show(500).animate({ opacity: 1 }, 3000).hide(500).animate({ opacity: 0 }, { duration: 0, complete: function () {
                             $('.dead__four').show(500).animate({ opacity: 1 }, 3000).hide(500).animate({ opacity: 0 }, { duration: 0, complete: function () { 
                                 $('.dead__five').show(500).animate({ opacity: 1 }, 3000).hide(500).animate({ opacity: 0 }, { duration: 0, complete: function () { 
-                                    $('.dead__boobs').show(500).animate({ opacity: 1 }, 3000).hide(500).animate({ opacity: 0 }, { duration: 0, complete: function () { } });
-
+                                    $('.six').show(500).animate({ opacity: 1 }, 3000).hide(500).animate({ opacity: 0 }, { duration: 0, complete: function () { 
+                                        $('.seven').show(500).animate({ opacity: 1 }, 3000).hide(500).animate({ opacity: 0 }, { duration: 0, complete: function () { 
+                                            $('.eight').show(500).animate({ opacity: 1 }, 3000).hide(500).animate({ opacity: 0 }, { duration: 0, complete: function () { 
+                                                $('.nine').show(500).animate({ opacity: 1 }, { duration: 3000, complete: function () { 
+                                                    $('.ten').show(500).animate({ opacity: 1 }, 3000);
+                                                        $('html, body').animate({
+                                                            scrollTop: $(".ten").offset().top
+                                                        }, 2000);
+                                                }} );
+                                            } });
+                                        } });
+                                    } });
                                 } });
-
                             } });
-
                         } });
-
                     } });
-
                 } });
             }});
         }});
